1188eeff24fd763352e0dbaef1e5a41235e9041d
승엽 <tmdduqflfl@naver.com>
2024-07-03 17:46:23 +0900

    7/3 잘못알고있던 부분 삭제

--- demo/src/main/java/com/example/demo/model/jpa/JpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model.jpa;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
-
-public interface JpaRepository<T,ID>extends PagingAndSortingRepository<T, ID>, QueryByExampleExecutor<T> {
-
-}
